51ea35d74d64366ee167218609d9d10200600ca2
linaacrm <bianca.conrafo21@gmail.com>
2025-11-25 19:25:12 -0400

    feature/modify type

---
+++ operadores_basicos/operadores.py
@@ -0,0 +1,28 @@
+inteiro = 10
+decimal = 3.14
+complexo = 3 +4j
+
+print(inteiro, decimal, complexo)
+print(f"Tipos: {type(inteiro)}, {type(decimal)}, type{(complexo)}")
+
+#Texto
+Texto = "Olá mundo"
+print(Texto)
+print(f"Tipos: {type(Texto)}")
+
+#Booleano
+
+verdadeiro = True
+falso = False
+print(verdadeiro)
+print(falso)
+print(f"Tipos: {type(verdadeiro)}, {type(falso)}")
+
+# coleções
+
+lista = [1, 2, 3]
+tupla = (1, 2, 3)
+dicionario = {"nome": "Marcos"}
+conjunto = {1, 2, 3}
+print(lista, tupla, dicionario, conjunto)
+print(f"Tipos: {type(lista)}, {type(tupla), type(dicionario), type(conjunto)}")
